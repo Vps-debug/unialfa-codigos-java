40969f6f8b1672a20bbcaf697793ccf600fc1aa3
Vps-debug <voltz098ps@gmail.com>
2025-08-29 00:50:16 -0300

    adicionando primeiro java

---
+++ Calculadora-IMC.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Calculadora-IMC {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Peso (kg): ");
+    double p = Double.parseDouble(s.next().replace(',', '.'));
+    
+    System.out.print("Altura (m): ");
+    double a = Double.parseDouble(s.next().replace(',', '.'));
+    
+    double imc = p / (a * a);
+    System.out.printf("IMC: %.2f%nClassificação: ", imc);
+    
+    System.out.println(
+      imc < 18.5 ? "Abaixo do peso" :
+      imc < 24.9 ? "Peso normal" :
+      imc < 29.9 ? "Sobrepeso" :
+      imc < 34.9 ? "Obesidade I" :
+      imc < 39.9 ? "Obesidade II" : "Obesidade III"
+    );
+
+    s.close();
+  }
+}
