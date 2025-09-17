9dd9906984eede785bea8d7032ef994d1c9ea45b
Vps-debug <voltz098ps@gmail.com>
2025-09-17 19:42:05 -0300

    Create VersaoJava.java

---
+++ Poo_Java/Atividade27_08/VersaoJava.java
@@ -0,0 +1,7 @@
+public class VersaoJava {
+    public static void main (String[] args){
+        String versaoJava = System.getProperty("java.version");
+        
+        System.out.println("versão do Java" + versaoJava);
+    }
+}
