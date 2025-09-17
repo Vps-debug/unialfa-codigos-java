0baa153f733b72ae70428284e5f9168898aa3bf1
Vps-debug <voltz098ps@gmail.com>
2025-09-17 19:44:00 -0300

    Delete Poo_Java/VersaoJava.java

--- Poo_Java/VersaoJava.java
+++
@@ -1,7 +0,0 @@
-public class VersaoJava {
-    public static void main (String[] args){
-        String versaoJava = System.getProperty("java.version");
-        
-        System.out.println("versão do Java" + versaoJava);
-    }
-}
