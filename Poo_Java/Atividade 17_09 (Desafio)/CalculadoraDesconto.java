ba360290c741024d1a83454f11d4c352c4982131
Vps-debug <voltz098ps@gmail.com>
2025-09-17 21:17:47 -0300

    Update CalculadoraDesconto.java

--- Poo_Java/Atividade 17_09 (Desafio)/CalculadoraDesconto.java
+++ Poo_Java/Atividade 17_09 (Desafio)/CalculadoraDesconto.java
@@ -6,21 +6,16 @@ public class CalculadoraDesconto {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-           
             System.out.print("Digite o valor do produto: ");
             double valorProduto = Double.parseDouble(reader.readLine());
 
-            
             System.out.print("Digite o percentual de desconto: ");
             double percentualDesconto = Double.parseDouble(reader.readLine());
 
-            
             double valorDesconto = valorProduto * percentualDesconto / 100;
 
-            
             double valorComDesconto = valorProduto - valorDesconto;
 
-            
             System.out.printf("Valor do desconto: R$ %.2f\n", valorDesconto);
             System.out.printf("Valor do produto com desconto: R$ %.2f\n", valorComDesconto);
 
