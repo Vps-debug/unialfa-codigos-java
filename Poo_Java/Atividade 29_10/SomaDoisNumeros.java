4963715d30d4f4909ea0a035fdea9f5d3ec9d8d1
Vps-debug <voltz098ps@gmail.com>
2025-10-29 20:41:23 -0300

    Create SomaDoisNumeros.java

---
+++ Poo_Java/Atividade 29_10/SomaDoisNumeros.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+public class SomaDoisNumero {
+    public static void main(String[] args) {
+int a = 1;
+int b = 1000;
+int resultado;
+resultado = a+b;
+System.out.println("O resultado da soma será" +resultado);
+    } 
+}
