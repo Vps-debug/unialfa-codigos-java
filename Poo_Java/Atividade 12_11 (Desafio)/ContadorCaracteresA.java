68271746876272c721adff42a950709944a878ad
Vps-debug <voltz098ps@gmail.com>
2025-11-12 20:25:39 -0300

    Update ContadorCaracteresA.java

--- Poo_Java/Atividade 12_11 (Desafio)/ContadorCaracteresA.java
+++ Poo_Java/Atividade 12_11 (Desafio)/ContadorCaracteresA.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+
+public class ContadorCaracteresA {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite uma frase: ");
+		String frase = entrada.nextLine();
+		
+		int contador = 0;
+		
+		for (int i = 0; i < frase.length(); i++) {
+		    char c = frase.charAt(i);
+		    
+		    if (c == 'a' || c == 'A') {
+		        contador++;
+		    }
+		}
+		
+		System.out.println("A letra 'a' aparece " + contador + " vezes na frase.");
+		
+		entrada.close();
+	}
+}
